b762a89063a83a6a9a3f9eb89c357af30fa2e37c
Daniel <danielweissenborn@outlook.de>
2021-07-01 19:20:52 +0200

    good ideas but bad execution2

--- Task2/Task2/src/TypeChecker.java
+++ Task2/Task2/src/TypeChecker.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertFalse;
 public class TypeChecker extends  ExprPLBaseListener{
 
     private ExprPLParser.ExprContext tree;
-    public static List<String> valuelist = new ArrayList<String>();
+    public static Boolean typechecker = false;
     public TypeChecker(ExprPLParser.ExprContext tree) {
         this.tree = tree;
     }
@@ -18,79 +18,62 @@ public class TypeChecker extends  ExprPLBaseListener{
     @Override
     public void exitExpr(ExprPLParser.ExprContext ctx) {
         if(ctx.getChild(0)==ctx.True()){
-            valuelist.add("true");
-            System.out.println("true");
+            //valuelist.add("true");
+            //System.out.println("true");
+            typechecker = true;
         }
-        if(ctx.getChild(0)==ctx.False()){
-            valuelist.add("false");
+        else if(ctx.getChild(0)==ctx.False()){
+            typechecker = false;
         }
-        if(ctx.getChild(0)==ctx.Not()){
-            valuelist.add("!" + ctx.getChild(1).toString());
+        else if(ctx.getChild(0)==ctx.Not()){
+            //valuelist.add("!" + ctx.getChild(1).toString());
         }
-        if (ctx.getChild(0) == ctx.Number()) {
-            valuelist.add(ctx.getChild(0).toString());
+        else   if(ctx.getChild(0) == ctx.Number()) {
+            //valuelist.add(ctx.getChild(0).toString());
         }
-        if (ctx.getChild(1) == ctx.Plus()) {
-            valuelist.add(String.valueOf(Integer.parseInt(ctx.getChild(0).toString()) + Integer.parseInt(ctx.getChild(1).toString())));
+        else   if (ctx.getChild(1) == ctx.Plus()) {
+            //valuelist.add(String.valueOf(Integer.parseInt(ctx.getChild(0).toString()) + Integer.parseInt(ctx.getChild(1).toString())));
         }
-        if (ctx.getChild(1) == ctx.Equal()) {
-            valuelist.add(String.valueOf(ctx.getChild(0).toString().equals(ctx.getChild(1).toString())));
+        else    if (ctx.getChild(1) == ctx.Equal()) {
+            //valuelist.add(String.valueOf(ctx.getChild(0).toString().equals(ctx.getChild(1).toString())));
         }
-        if (ctx.getChild(1) == ctx.And()) {
-            valuelist.add(String.valueOf((Boolean.parseBoolean(ctx.getChild(0).toString()) && (Boolean.parseBoolean(ctx.getChild(1).toString())))));
+        else   if (ctx.getChild(1) == ctx.And()) {
+            //valuelist.add(String.valueOf((Boolean.parseBoolean(ctx.getChild(0).toString()) && (Boolean.parseBoolean(ctx.getChild(1).toString())))));
         }
-        if (ctx.getChild(0) == ctx.If() ) {
+        else   if (ctx.getChild(0) == ctx.If() ) {
             if(Boolean.parseBoolean(ctx.getChild(1).toString())){
-                valuelist.add(ctx.getChild(3).toString());
+               // valuelist.add(ctx.getChild(3).toString());
             }else{
-                valuelist.add(ctx.getChild(5).toString());
+                //valuelist.add(ctx.getChild(5).toString());
             }
         }
     }
 
-    @Override
-    public void visitTerminal(TerminalNode node) {
-        super.visitTerminal(node);
-    }
-
     public boolean check() {
-        // TODO: implement
-        // literal --int
-        // booleans --true|false
-        // boolean negation of integer  --!int
-        //conjunction &&
-        //Addition checker +
-        //integer equality =
-        //if e then e else e --if e == true {e} else e
-        //exitExpr(tree);
         ExprPLBaseListener listener = new ExprPLBaseListener();
         ParseTreeWalker walker = new ParseTreeWalker();
-        walker.walk(listener, this.tree);
-
-
-       // System.out.print(valuelist);
-        //System.out.println(stack.peek());
-
-        return false;
+        walker.walk(listener, tree);
+        return typechecker;
     }
-    public List<String> run() {
-        List<String> output = new ArrayList<>();
-        valuelist.clear();
+
+    public Boolean run() {
+        Boolean output;
+       // valuelist.clear();
         ExprPLBaseListener listener = new ExprPLBaseListener();
         ParseTreeWalker walker = new ParseTreeWalker();
         walker.walk(listener, tree);
         //System.out.println("values of method: " + lister());
-        output = valuelist;
+        output = typechecker;
         //System.out.println("values of original: " + outputvalues);
         //System.out.println("values of output: " + output);
         return output;
     }
     public static void main(String[] args){
-        String code = "true && true";
+        String code = "true";
         ExprPLParser.ExprContext tree = Util.parseCode(code);
         TypeChecker checker = new TypeChecker(tree);
         assertFalse(checker.check());
-        List<String> output = checker.run();
+        Boolean output = checker.run();
         System.out.println(output);
     }
 }
